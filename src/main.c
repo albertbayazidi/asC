e8cd767587bc2fab84371174ec03db348ea22baf
Albert <albert.bayazidi@gmail.com>
2024-07-29 20:28:11 +0200

    simple testing

--- src/main.c
+++ src/main.c
@@ -1,7 +1,16 @@
 #include <math.h>
 #include <stdio.h>
+#include <unistd.h>
+#include "../lib/utils.h"
+
+#define width 160
+#define height 44
+
+float cubeWidth = 10;
+char buffer[width*height];
 
 int main(){
-    printf("%f",sqrt(10));
+    printf("Here we have the acctaul size of the matrix: %d \n",width*height);
+    outputVec(buffer);
 return 0;
 }
