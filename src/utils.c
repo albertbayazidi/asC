a034c421b7c9a6fbf5d463a0acf4c673439d9833
Albert <albert.bayazidi@gmail.com>
2024-08-11 12:51:54 +0200

    removed unused function

--- src/utils.c
+++ src/utils.c
@@ -22,14 +22,4 @@ char* makeDocFromReadme(){
 }
 
 
-void printHelp(int argc, char **argv) {
-    if (argc > 1 && strcmp(argv[argc - 1], "-h") == 0) {
-        printf("  --path     / -p        path to image\n");
-        printf("  --resize   / -r        resize Factor (optional)\n");
-        printf("  --invert  / -i        invert image color (optional)\n");
-        printf("  --help     / -h        this information\n");
-    }
-}
-
-
 
