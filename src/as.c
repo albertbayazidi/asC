47f40bbc890c2bceccdd33c277f5a1d7605f90b3
Albert <albert.bayazidi@gmail.com>
2024-09-28 11:22:37 +0200

    update

--- src/as.c
+++ src/as.c
@@ -1,15 +1,15 @@
 #include "../lib/asc.h"
 
-void printSimpleAsc(unsigned char* data, int* params, char* density){
-for (int y = 0; y < params[1]; y++) {
-        for (int x = 0; x < params[0]; x++) {
-            int r = data[(y * params[0] + x) * params[2] + 0];
-            int g = data[(y * params[0] + x) * params[2] + 1];
-            int b = data[(y * params[0] + x) * params[2] + 2];
-            float gray = 0.2126f * r + 0.7152f * g + 0.0722f * b;
-            int index = (int)(gray / 255.0f * (strlen(density) - 1));
-            printf("%c", density[index]);
-        }
-        printf("\n");
+void printSimpleAsc(unsigned char *data, int *params, char *density) {
+  for (int y = 0; y < params[1]; y++) {
+    for (int x = 0; x < params[0]; x++) {
+      int r = data[(y * params[0] + x) * params[2] + 0];
+      int g = data[(y * params[0] + x) * params[2] + 1];
+      int b = data[(y * params[0] + x) * params[2] + 2];
+      float gray = 0.2126f * r + 0.7152f * g + 0.0722f * b;
+      int index = (int)(gray / 255.0f * (strlen(density) - 1));
+      printf("%c", density[index]);
     }
+    printf("\n");
+  }
 }
--- src/main.c
+++ src/main.c
@@ -1,42 +1,42 @@
 #define STB_IMAGE_IMPLEMENTATION
 #define STB_IMAGE_RESIZE_IMPLEMENTATION
 
-#include "../lib/imageUtils.h"
 #include "../lib/asc.h"
 #include "../lib/handleInputs.h"
+#include "../lib/imageUtils.h"
 
 const char *argp_program_version = "asC 0.1";
 const char *argp_program_bug_address = "<albert.bayazidi@gmail.com>";
-
 // width, height, channels
 int params[3];
-char* density = "      '.,=+:;!1?abcdefg0$#@N";
+char *density = "      '.,=+:;!1?abcdefg0$#@N";
 
-int main(int argc, char **argv){
+int main(int argc, char **argv) {
 
-    struct arguments arguments;
+  struct arguments arguments;
 
-    arguments.args[0] = NULL;
-    arguments.invert = 0;
-    arguments.reszie_factor = 1.0;
+  arguments.args[0] = NULL;
+  arguments.invert = 0;
+  arguments.reszie_factor = 1.0;
 
-    struct argp argp = initialize_doc();
-    argp_parse(&argp, argc, argv, 0, 0, &arguments);
+  struct argp argp = initialize_doc();
+  argp_parse(&argp, argc, argv, 0, 0, &arguments);
 
-    if (arguments.invert) {
-        density = "N@#$0gfedcba?1!;:+=,.'      ";
-    }
+  if (arguments.invert) {
+    density = "N@#$0gfedcba?1!;:+=,.'      ";
+  }
 
-    unsigned char* img_data = loadImage(arguments.args[0],params);
+  unsigned char *img_data = loadImage(arguments.args[0], params);
 
-    unsigned char* resizedImage = resizeImage(arguments.reszie_factor, img_data, params);
-    free(img_data);
-    img_data = resizedImage;
+  unsigned char *resizedImage =
+      resizeImage(arguments.reszie_factor, img_data, params);
+  free(img_data);
+  img_data = resizedImage;
 
-    printSimpleAsc(img_data,params,density);
+  printSimpleAsc(img_data, params, density);
 
-    //freeing mem
-    free(img_data);
+  // freeing mem
+  free(img_data);
 
-    return 0;
+  return 0;
 }
